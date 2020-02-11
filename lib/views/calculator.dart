9fb4c944ecae3bdf32316d1a1211db30519bdb5c
Kenneth Anderson <eucliwood98@gmail.com>
2020-02-11 17:05:07 +0800

    Boilerplate

---
+++ lib/views/calculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
