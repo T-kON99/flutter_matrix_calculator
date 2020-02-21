c81eeb0556b22511b014eb9c39f39cdf3d515b0e
Kenneth Anderson <eucliwood98@gmail.com>
2020-02-21 22:51:36 +0800

    Implemented theme to the NumberPicker widget. Added matrixName as an argument to be passed from parent widget to this widget in order to use. Refactored old dataTable view into a new method.

--- lib/form/matrix_form.dart
+++ lib/form/matrix_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:matrix_calculator/classes/matrix.dart';
 import 'package:numberpicker/numberpicker.dart';
 
@@ -8,8 +9,10 @@ typedef void MatrixCallback(Matrix matrix, String name);
 class MatrixAddForm extends StatefulWidget {
   final Matrix matrix;
   final MatrixCallback callback;
+  final String matrixName;
 
-  MatrixAddForm({Key key, this.matrix, this.callback}) : super(key: key);
+  MatrixAddForm({Key key, this.matrix, this.callback, this.matrixName})
+      : super(key: key);
   @override
   _MatrixAddFormState createState() => _MatrixAddFormState();
 }
@@ -19,7 +22,6 @@ class _MatrixAddFormState extends State<MatrixAddForm> {
   final matrixNameController = TextEditingController();
   int _minSize = 1;
   int _maxSize = 20;
-  int _curCol, _curRow;
 
   @override
   void dispose() {
@@ -29,6 +31,8 @@ class _MatrixAddFormState extends State<MatrixAddForm> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    matrixNameController.text = widget.matrixName;
     return Form(
         key: _formKey,
         child: Column(
@@ -44,7 +48,9 @@ class _MatrixAddFormState extends State<MatrixAddForm> {
                   );
                 },
               ),
-              title: Text('Add Matrix'),
+              title: this.widget.matrixName == null
+                  ? Text('Add Matrix')
+                  : Text('Editing Matrix'),
               centerTitle: true,
               actions: <Widget>[
                 Builder(
@@ -53,8 +59,10 @@ class _MatrixAddFormState extends State<MatrixAddForm> {
                       icon: Icon(Icons.save),
                       tooltip: 'Save Matrix',
                       onPressed: () {
+                        print('Validating...');
                         if (_formKey.currentState.validate()) {
                           _formKey.currentState.save();
+                          print('Saving Matrix ${matrixNameController.text}');
                           this.widget.callback(
                               this.widget.matrix, matrixNameController.text);
                           Navigator.pop(context);
@@ -81,83 +89,140 @@ class _MatrixAddFormState extends State<MatrixAddForm> {
                 },
               ),
               subtitle: ListTile(
-                title: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    NumberPicker.integer(
-                      initialValue: this.widget.matrix.col,
-                      minValue: _minSize,
-                      maxValue: _maxSize,
-                      onChanged: (value) => this.setState(() {
-                        _curCol = value;
-                      }),
-                    ),
-                    Text('X'),
-                    NumberPicker.integer(
-                      initialValue: this.widget.matrix.row,
-                      minValue: _minSize,
-                      maxValue: _maxSize,
-                      onChanged: (value) =>
-                          this.setState(() => _curRow = value),
-                    )
-                  ],
-                ),
-                subtitle: DataTable(
-                    columns: List.from(
-                        List.generate(this.widget.matrix.col, (index) => index)
-                            .map<DataColumn>((index) {
-                      return DataColumn(
-                          numeric: true,
-                          tooltip: 'Column ${index.toString()}',
-                          label: Text('Column ${index.toString()}'));
-                    })),
-                    rows: widget.matrix.data
-                        .asMap()
-                        .map((int colIndex, List<double> row) {
-                          return MapEntry(
-                            colIndex,
-                            DataRow(
-                                selected: false,
-                                cells: row
-                                    .asMap()
-                                    .map((int rowIndex, double value) {
-                                      return MapEntry(
-                                          rowIndex,
-                                          DataCell(
-                                            TextFormField(
-                                              initialValue: value.toString(),
-                                              validator: (value) {
-                                                if (value.isEmpty) {
-                                                  return 'Enter a value';
-                                                } else if (double.tryParse(
-                                                        value) ==
-                                                    null) {
-                                                  return 'Enter a valid number';
-                                                }
-                                                return null;
-                                              },
-                                              onSaved: (value) {
-                                                if(_formKey.currentState.validate()) {
-                                                  this.widget.matrix.data[rowIndex][colIndex] = double.parse(value);
-                                                }
-                                              },
-                                            ),
-                                            showEditIcon: true,
-                                            placeholder: true,
-                                            onTap: () {
-                                              print('Editing');
-                                            },
-                                          ));
-                                    })
-                                    .values
-                                    .toList()),
-                          );
-                        })
-                        .values
-                        .toList()),
-              ),
+                  title: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'Size',
+                          textScaleFactor: 0.9,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            //  TODO: Better UI
+                            Theme(
+                                data: theme.copyWith(
+                                    //  Highlighted text color
+                                    accentColor: Colors.blue,
+                                    textTheme: theme.textTheme.copyWith(
+                                      headline: theme.textTheme.headline
+                                          .copyWith(), //  Other styles for highlighted
+                                      body1: theme.textTheme.headline.copyWith(
+                                        fontSize: 10,
+                                      ), // Not highlighted styles
+                                    )),
+                                child: NumberPicker.integer(
+                                  initialValue: this.widget.matrix.row,
+                                  minValue: _minSize,
+                                  maxValue: _maxSize,
+                                  itemExtent: 25,
+                                  onChanged: (value) => this.setState(() {
+                                    this.widget.matrix.zeroFillResize(
+                                        row: value,
+                                        col: this.widget.matrix.col);
+                                  }),
+                                )),
+                            Text('X'),
+                            Theme(
+                                data: theme.copyWith(
+                                    //  Highlighted text color
+                                    accentColor: Colors.blue,
+                                    textTheme: theme.textTheme.copyWith(
+                                      headline: theme.textTheme.headline
+                                          .copyWith(), //  Other styles for highlighted
+                                      body1: theme.textTheme.headline.copyWith(
+                                        fontSize: 10,
+                                      ), // Not highlighted styles
+                                    )),
+                                child: NumberPicker.integer(
+                                  initialValue: this.widget.matrix.col,
+                                  minValue: _minSize,
+                                  maxValue: _maxSize,
+                                  itemExtent: 25,
+                                  onChanged: (value) => this.setState(() {
+                                    this.widget.matrix.zeroFillResize(
+                                        row: this.widget.matrix.row,
+                                        col: value);
+                                  }),
+                                )),
+                          ],
+                        ),
+                        Divider(
+                          color: Colors.black,
+                        )
+                      ]),
+                  subtitle: SingleChildScrollView(
+                      padding: EdgeInsets.all(0),
+                      scrollDirection: Axis.horizontal,
+                      child: Center(
+                        child: buildMatrixDataTable(),
+                      ))),
             ),
           ],
         ));
+  }
+
+  DataTable buildMatrixDataTable() {
+    return DataTable(
+        columnSpacing: 10,
+        columns: List.from(
+            List.generate(this.widget.matrix.col, (index) => index)
+                .map<DataColumn>((index) {
+          return DataColumn(
+              numeric: true,
+              //  Show it in 1-index based for human readability
+              tooltip: 'Column ${(index + 1).toString()}',
+              label: Text(
+                'Column ${(index + 1).toString()}',
+                textScaleFactor: 1.25,
+              ));
+        })),
+        rows: this
+            .widget
+            .matrix
+            .data
+            .asMap()
+            .map((int rowIndex, List<double> row) {
+              return MapEntry(
+                rowIndex,
+                DataRow(
+                    selected: false,
+                    cells: row
+                        .asMap()
+                        .map((int colIndex, double value) {
+                          return MapEntry(
+                              colIndex,
+                              DataCell(
+                                TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  initialValue: value.toString(),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Enter a value';
+                                    } else if (double.tryParse(value) == null) {
+                                      return 'Enter a valid number';
+                                    }
+                                    return null;
+                                  },
+                                  onSaved: (value) {
+                                    if (_formKey.currentState.validate()) {
+                                      this.setState(() =>
+                                          this.widget.matrix.data[rowIndex]
+                                              [colIndex] = double.parse(value));
+                                    }
+                                  },
+                                ),
+                                showEditIcon: true,
+                                placeholder: true,
+                              ));
+                        })
+                        .values
+                        .toList()),
+              );
+            })
+            .values
+            .toList());
   }
 }
