8a438455324ce828a7e0ca3d79762bf7ad56496c
Kenneth Anderson <eucliwood98@gmail.com>
2020-07-07 05:39:47 +0800

    Implemented WIP partial calculator page. Now is showing all available operations.

--- lib/views/calculator.dart
+++ lib/views/calculator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../classes/matrix.dart';
+import '../form/matrix_operation.dart';
 
 class CalculatorPage extends StatefulWidget {
   final Map<String, Matrix> data;
@@ -13,19 +14,22 @@ class CalculatorPage extends StatefulWidget {
 //  1.  Have preset of functions available which user can select (Ex: Add, Multiplication, Inverse, GE, RE, RRE)
 //  2.  If (1) finishes, maybe let another field which is "Custom". This will behave like MATLAB (Ex: Users able to input: det(mat(A)*mat(B))
 class _CalculatorPageState extends State<CalculatorPage> {
-  List<String> actions = ["add"];
+  final List<MatrixOperationForm> operations = MatrixOperations;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: this.actions.length,
-        itemBuilder: (BuildContext context, i) {
-          return Center(
-            child: Card(
-              child: Text(this.actions[i]),
-            ),
-          );
-      }),
+          itemCount: this.operations.length,
+          itemBuilder: (BuildContext context, i) {
+            return Center(
+              child: Card(
+                child: OperationFormView(
+                  data: widget.data,
+                  operation: this.operations[i],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
