29885fb94c11e40d538646e9814550863dc4a8ab
Kenneth Anderson <eucliwood98@gmail.com>
2020-07-20 20:43:03 +0800

    Added a `needMatrix` field. This is needed to implement operations that argument can either be a matrix or scalar value.

--- lib/form/matrix_operation.dart
+++ lib/form/matrix_operation.dart
@@ -5,14 +5,15 @@ import '../classes/matrix.dart';
 import '../presentation/custom_icon_icons.dart' show CustomIcon;
 
 //  TODO: Finish this form. HALFWAY
-class MatrixOperationForm {
-  const MatrixOperationForm(this.title, this.icon, this.color, this.bgColor,
-      this.singleOperation, this.name, this.description);
+class MatrixOperation {
+  const MatrixOperation(this.title, this.icon, this.color, this.bgColor,
+      this.singleOperation, this.needMatrix, this.name, this.description);
   final String title;
   final IconData icon;
   final Color color;
   final Color bgColor;
   final bool singleOperation;
+  final bool needMatrix;
   final String name;
   final String description;
 }
@@ -20,7 +21,7 @@ class MatrixOperationForm {
 class OperationFormView extends StatefulWidget {
   const OperationFormView({Key key, this.operation, this.data})
       : super(key: key);
-  final MatrixOperationForm operation;
+  final MatrixOperation operation;
   final Map<String, Matrix> data;
   @override
   _OperationFormViewState createState() => _OperationFormViewState();
@@ -52,7 +53,7 @@ class _OperationFormViewState extends State<OperationFormView> {
 //  TODO
 Card buildCard(
     {Key key,
-    MatrixOperationForm widget,
+    MatrixOperation widget,
     BuildContext context,
     Map<String, Matrix> data}) {
   return Card(
@@ -86,6 +87,7 @@ Card buildCard(
                     title: widget.title,
                     name: widget.name,
                     singleOperation: widget.singleOperation,
+                    needMatrix: widget.needMatrix,
                     data: data
                   );
                 })
@@ -97,77 +99,86 @@ Card buildCard(
 }
 
 //  TODO: Give appropriate icons
-final List<MatrixOperationForm> MatrixOperations = <MatrixOperationForm>[
-  MatrixOperationForm(
+final List<MatrixOperation> MatrixOperations = <MatrixOperation>[
+  MatrixOperation(
       'Addition',
       CustomIcon.plus,
       Colors.yellow,
       Colors.green,
       false,
+      true,
       'add',
       'Perform addition of Matrix 1 + Matrix 2. Dimension of both Matrixes must be exactly the same'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Substraction',
       CustomIcon.minus,
       Colors.yellow,
       Colors.blue,
       false,
+      true,
       'sub',
       'Perform subtraction of Matrix 1 - Matrix 2. Dimension of both Matrixes must be exactly the same'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Matrix Multiplication',
       CustomIcon.asterisk,
       Colors.green[200],
       Colors.red,
       false,
+      true,
       'mmult',
       'Perform multiplication of a Matrix 1 * Matrix 2. Dimension of both Matrixes must obey m x n * n x k.'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Scalar Multiplication',
       CustomIcon.cancel,
       Colors.green[200],
       Colors.cyan,
       false,
+      false,
       'smult',
       'Perform scalar multiplication of Matrix * Decimal Number'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Power',
       CustomIcon.angle_up,
       Colors.blue[300],
       Colors.yellow[700],
-      true,
+      false,
+      false,
       'pow',
       'Perform power operation. Example would be (Matrix 1)^(Integer Power). A power of -1 will be calculating its inverse.'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Determinant',
       CustomIcon.eject,
       Colors.blue[300],
       Colors.orange,
       true,
+      false,
       'det',
       'Calculate the determinant of the given Matrix. Given Matrix must be a square Matrix'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Inverse',
       CustomIcon.info,
       Colors.green[100],
       Colors.pink,
       true,
+      false,
       'inv',
       'Calculate the inverse of the given Matrix. Given Matrix must be a square Matrix'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Row Echelon Form',
       CustomIcon.yandex,
       Colors.teal[200],
       Colors.teal,
       true,
+      false,
       'RE',
       'Calculate the Row Echelon Form (RE) of the given Matrix.'),
-  MatrixOperationForm(
+  MatrixOperation(
       'Reduced Row Echelon Form',
       CustomIcon.calc,
       Colors.deepPurple[200],
       Colors.purple,
       true,
+      false,
       'RRE',
       'Calculate the Reduced Row Echelon Form (RRE) of the given Matrix')
 ];
