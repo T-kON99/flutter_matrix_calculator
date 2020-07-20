eaab7d184d7bb610fdc2598260edcaac736d4068
Kenneth Anderson <eucliwood98@gmail.com>
2020-07-20 20:43:19 +0800

    Renamed.

--- lib/views/calculator.dart
+++ lib/views/calculator.dart
@@ -14,7 +14,7 @@ class CalculatorPage extends StatefulWidget {
 //  1.  Have preset of functions available which user can select (Ex: Add, Multiplication, Inverse, GE, RE, RRE)
 //  2.  If (1) finishes, maybe let another field which is "Custom". This will behave like MATLAB (Ex: Users able to input: det(mat(A)*mat(B))
 class _CalculatorPageState extends State<CalculatorPage> {
-  final List<MatrixOperationForm> operations = MatrixOperations;
+  final List<MatrixOperation> operations = MatrixOperations;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
