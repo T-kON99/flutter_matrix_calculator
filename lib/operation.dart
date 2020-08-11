6b79c88062fd51b8433023bffd4c4acee21896fb
Kenneth Anderson <eucliwood98@gmail.com>
2020-08-12 05:57:08 +0800

    Changed name from Operations to Operation. Updated to better structure. Now using enum directly instead of string.

--- lib/operation.dart
+++ lib/operation.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'presentation/custom_icon_icons.dart' show CustomIcon;
 
-enum Operations {
+enum Operation {
   ADD,
   SUB,
   MATRIX_MULT,
@@ -15,29 +15,29 @@ enum Operations {
   RRE
 }
 
-extension OperationsExt on Operations {
+extension OperationsExt on Operation {
   static const operationShortName = {
-    Operations.ADD: "add",
-    Operations.SUB: "sub",
-    Operations.SCALAR_MULT: "smult",
-    Operations.MATRIX_MULT: "mmult",
-    Operations.POW: "pow",
-    Operations.DET: "det",
-    Operations.INV: "inv",
-    Operations.RE: "RE",
-    Operations.RRE: "RRE",
+    Operation.ADD: "add",
+    Operation.SUB: "sub",
+    Operation.SCALAR_MULT: "smult",
+    Operation.MATRIX_MULT: "mmult",
+    Operation.POW: "pow",
+    Operation.DET: "det",
+    Operation.INV: "inv",
+    Operation.RE: "RE",
+    Operation.RRE: "RRE",
   };
 
   static const operationFullName = {
-    Operations.ADD: "Addition",
-    Operations.SUB: "Subtraction",
-    Operations.SCALAR_MULT: "Scalar Multiplication",
-    Operations.MATRIX_MULT: "Matrix Multiplication",
-    Operations.POW: "Power",
-    Operations.DET: "Determinant",
-    Operations.INV: "Inverse",
-    Operations.RE: "Row Echelon Form",
-    Operations.RRE: "Reduced Row Echelon Form",
+    Operation.ADD: "Addition",
+    Operation.SUB: "Subtraction",
+    Operation.SCALAR_MULT: "Scalar Multiplication",
+    Operation.MATRIX_MULT: "Matrix Multiplication",
+    Operation.POW: "Power",
+    Operation.DET: "Determinant",
+    Operation.INV: "Inverse",
+    Operation.RE: "Row Echelon Form",
+    Operation.RRE: "Reduced Row Echelon Form",
   };
   
   String get shortName => operationShortName[this];
@@ -45,117 +45,97 @@ extension OperationsExt on Operations {
 }
 
 class MatrixOperation {
-  const MatrixOperation(this.title, this.icon, this.color, this.bgColor,
-      this.singleOperation, this.needMatrix, this.name, this.description, this.enumOperation);
-  final String title;
+  const MatrixOperation(this.operation, this.icon, this.color, this.bgColor,
+      this.singleOperation, this.needMatrix, this.description);
+  final Operation operation;
   final IconData icon;
   final Color color;
   final Color bgColor;
   final bool singleOperation;
   final bool needMatrix;
-  final String name;
   final String description;
-  final Operations enumOperation;
 }
 
 final List<MatrixOperation> MatrixOperations = <MatrixOperation>[
   MatrixOperation(
-      Operations.ADD.fullName,
+      Operation.ADD,
       CustomIcon.plus,
       Colors.yellow,
       Colors.green,
       false,
       true,
-      Operations.ADD.shortName,
       'Perform addition of Matrix 1 + Matrix 2. Dimension of both Matrixes must be exactly the same',
-      Operations.ADD
   ),
   MatrixOperation(
-      Operations.SUB.fullName,
+      Operation.SUB,
       CustomIcon.minus,
       Colors.yellow,
       Colors.blue,
       false,
       true,
-      Operations.SUB.shortName,
       'Perform subtraction of Matrix 1 - Matrix 2. Dimension of both Matrixes must be exactly the same',
-      Operations.SUB
   ),
   MatrixOperation(
-      Operations.MATRIX_MULT.fullName,
+      Operation.MATRIX_MULT,
       CustomIcon.asterisk,
       Colors.green[200],
       Colors.red,
       false,
       true,
-      Operations.MATRIX_MULT.shortName,
       'Perform multiplication of a Matrix 1 * Matrix 2. Dimension of both Matrixes must obey m x n * n x k.',
-      Operations.MATRIX_MULT,
   ),
   MatrixOperation(
-      Operations.SCALAR_MULT.fullName,
+      Operation.SCALAR_MULT,
       CustomIcon.cancel,
       Colors.green[200],
       Colors.cyan,
       false,
       false,
-      Operations.SCALAR_MULT.shortName,
       'Perform scalar multiplication of Matrix * Decimal Number',
-      Operations.SCALAR_MULT,
   ),
   MatrixOperation(
-      Operations.POW.fullName,
+      Operation.POW,
       CustomIcon.angle_up,
       Colors.blue[300],
       Colors.yellow[700],
       false,
       false,
-      Operations.POW.shortName,
       'Perform power operation. Example would be (Matrix 1)^(Integer Power). A power of -1 will be calculating its inverse.',
-      Operations.POW,
   ),
   MatrixOperation(
-      Operations.DET.fullName,
+      Operation.DET,
       CustomIcon.eject,
       Colors.blue[300],
       Colors.orange,
       true,
       false,
-      Operations.DET.shortName,
       'Calculate the determinant of the given Matrix. Given Matrix must be a square Matrix',
-      Operations.DET,
   ),
   MatrixOperation(
-      Operations.INV.fullName,
+      Operation.INV,
       CustomIcon.info,
       Colors.green[100],
       Colors.pink,
       true,
       false,
-      Operations.INV.shortName,
       'Calculate the inverse of the given Matrix. Given Matrix must be a square Matrix',
-      Operations.INV,
   ),
   MatrixOperation(
-      Operations.RE.fullName,
+      Operation.RE,
       CustomIcon.yandex,
       Colors.teal[200],
       Colors.teal,
       true,
       false,
-      Operations.RE.shortName,
       'Calculate the Row Echelon Form (RE) of the given Matrix.',
-      Operations.RE,
   ),
   MatrixOperation(
-      Operations.RRE.fullName,
+      Operation.RRE,
       CustomIcon.calc,
       Colors.deepPurple[200],
       Colors.purple,
       true,
       false,
-      Operations.RRE.shortName,
       'Calculate the Reduced Row Echelon Form (RRE) of the given Matrix',
-      Operations.RRE,
   )
 ];
